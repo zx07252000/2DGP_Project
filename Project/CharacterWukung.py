a96e6f9f7c6cadd0b4e27d559792ecd4e95b2bd1
zx07252000 <zx07252000@naver.com>
2018-11-12 22:59:55 +0900

    시간함수 변경및 폰트 조절중
    2015180036 장영진 커밋횟수 71회
    오공 메이메이 시간함수 변경및 폰트 조절중

--- Project/CharacterWukung.py
+++ Project/CharacterWukung.py
@@ -4,6 +4,25 @@ from ball import Ball
 import game_world
 
 # Boy Event
+
+PIXEL_PER_METER = (10.0 / 2.0)
+
+RUN_SPEED_KMPH = 20.0
+
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
+TIME_PER_ACTION = 0.5
+
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+
+FRAMES_PER_ACTION = 8
+
+
 RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP,SPACE_E,UP_DOWN,DOWN_DOWN,UP_UP,DOWN_UP = range(9)
 
 key_event_table = {
@@ -27,21 +46,24 @@ class IdleState:
     @staticmethod
     def enter(Wukung, event):
         if event == RIGHT_DOWN:
-            Wukung.velocity += 30
+            Wukung.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
-            Wukung.velocity -= 30
+            Wukung.velocity -= RUN_SPEED_PPS
+
         elif event == RIGHT_UP:
-            Wukung.velocity -= 30
+            Wukung.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
-            Wukung.velocity += 30
+            Wukung.velocity += RUN_SPEED_PPS
+
         elif event == DOWN_DOWN:
-            Wukung.length -= 30
+            Wukung.length -= RUN_SPEED_PPS
         elif event == UP_DOWN:
-            Wukung.length += 30
+            Wukung.length += RUN_SPEED_PPS
+
         elif event == DOWN_UP:
-            Wukung.length += 30
+            Wukung.length += RUN_SPEED_PPS
         elif event == UP_UP:
-            Wukung.length -= 30
+            Wukung.length -= RUN_SPEED_PPS
 
 
 
@@ -74,20 +96,24 @@ class RunState:
     @staticmethod
     def enter(Wukung, event):
         if event == RIGHT_DOWN:
-            Wukung.velocity += 30
+            Wukung.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
-            Wukung .velocity -=30
+            Wukung .velocity -=RUN_SPEED_PPS
+
         elif event == RIGHT_UP:
-            Wukung.velocity -= 30
+            Wukung.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            Wukung.velocity += RUN_SPEED_PPS
 
         elif event == DOWN_DOWN:
-            Wukung.length -= 30
+            Wukung.length -= RUN_SPEED_PPS
         elif event == UP_DOWN:
-            Wukung.length += 30
+            Wukung.length += RUN_SPEED_PPS
+
         elif event == DOWN_UP:
-            Wukung.length += 30
+            Wukung.length += RUN_SPEED_PPS
         elif event == UP_UP:
-            Wukung.length -= 30
+            Wukung.length -= RUN_SPEED_PPS
 
         Wukung.dir = Wukung.velocity
 
@@ -135,6 +161,7 @@ class Wukung:
     def __init__(self):
         self.x, self.y = 70 , 70
         self.image = load_image('Resource_Character\\CharacterWukung3.png')
+        self.font = load_font('Resource_Temporary\\ENCR10B.TTF', 16)
         self.dir = 1
         self.velocity = 0
         self.frame = 0
@@ -165,7 +192,7 @@ class Wukung:
 
     def draw(self):
         self.cur_state.draw(self)
-
+        self.font.draw(self.x - 60, self.y + 50, '(Time:%3.2f)' % get_time(), (255, 255, 0))
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
